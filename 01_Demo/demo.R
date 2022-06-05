fe8d6ac6df223af2e556743fbf66b6858db9a139
Yuting Gao <yg2790@columbia.edu>
2022-06-05 19:22:36 +0800

    initial upload

---
+++ 01_Demo/demo.R
@@ -0,0 +1,14 @@
+# function_name <- function(arg1, arg2=T, ...){
+# <Body of the Function>
+# return <Return Value>
+# }
+
+# filter_table2 <- subset(demo_table2, price >= 1000 & drive == "4wd" & "clean" 
+# %in% title_status)
+# filter by price and drive
+
+
+# nrow(): counts the number of rows
+
+#hjust tells ggplot that our rotated labels should be aligned horizontally to 
+# our tick marks
